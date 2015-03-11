d478a4cb047a841b87382ff4467ee03ffa97ad50
czhouyi <czhouyi@gmail.com>
2015-03-11 17:04:08 +0800

    form表单交互

--- app/account/urls.py
+++ app/account/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, url
 from app.account.views import *
 
 urlpatterns = patterns('',
-        url(r'^sign_in/$',       sign_in,          name='sign_in'),
-        url(r'^do_sign_in/$',    do_sign_in,       name='do_sign_in'),
-        url(r'^sign_up/$',       sign_up,          name='sign_up'),
-        url(r'^do_sign_up/$',    do_sign_up,       name='do_sign_up'),
-        url(r'^change_pwd/$',    change_pwd,       name='change_pwd'),
-        url(r'^do_change_pwd/$', do_change_pwd,    name='do_change_pwd'),
-        url(r'^sign_out/$',      sign_out,         name='sign_out'),
+        url(r'^sign_in/$',              sign_in,                name='sign_in'),
+        url(r'^sign_up/$',              sign_up,                name='sign_up'),
+        url(r'^reset_password/$',       reset_password,         name='reset_password'),
+        url(r'^sign_out/$',             sign_out,               name='sign_out'),
+
+        url(r'^profile/$',              profile,                name='profile'),
+        url(r'^security/$',             security,               name='security'),
+
 )
